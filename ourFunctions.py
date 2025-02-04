8177614f7f64dd24d96a6d3d9d91c37355e1afd1
Danie Huelva <116598461+DanieHuelva@users.noreply.github.com>
2025-02-04 16:45:31 -0700

    Add files via upload

---
+++ ourFunctions.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+data = np.load('poker-hand-testing.data')
+
+def multi_mean(att1):
+    return 0
+
+
+def sampleVar(att1):
+    return 0
+
+
+def sampleCov(att1, att2):
+    return 0
+
+
+def covMatrix(data):
+    #can use sampleVar and sampleCov
+    return 0
+
+
+def correlationCoEf(att1, att2):
+    return 0
+
