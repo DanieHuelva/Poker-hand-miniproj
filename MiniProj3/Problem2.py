2f35f73369d14515e9a840ad158a7a3420f625f1
DanieHuelva <danshyn23@gmail.com>
2025-04-13 09:42:22 -0600

    starting proj3

--- MiniProj3/Problem2.py
+++ MiniProj3/Problem2.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
+import sys
 
 
 filename = 'Boston.csv'
@@ -12,8 +13,21 @@ scalar = StandardScaler()
 STD = scalar.fit_transform(matrix_data)
 
 
-#Problem 2.1
+
 def getKmeans(data, k, eps):
+    scalar = StandardScaler()
+    STD = scalar.fit_transform(data)
+    pca = PCA(n_components=13)
+    RPCA = pca.fit_transform(STD)
+    #Make random centroids
+    ## thhis is a looop!!1
+    #assign points to closest centroid 
+    #reassign centroid
+    #check if distance is less than eps
     return 0
 
-#Problem 2.2
+
+
+
+matrix_data = pd.read_csv("Customers.csv")
+getKmeans(matrix_data, 4, sys.float_info.epsilon)
